c93071887a69e1b756cfbc16358c9fe5cce3276c
Pinqua <piyushsati311999@gmail.com>
2022-03-02 22:05:48 +0530

    added test cases

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,17 @@
 
 from django.contrib import admin
 from .models import Choice, Question
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
